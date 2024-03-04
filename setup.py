b6f404f8d724c5a0f92d36f2bcdfb9172b05c1e9
Auric Vente <mprops@protonmail.com>
2024-03-04 02:56:06 -0600

    Mods

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "meltdown = main:main",
+            "meltdown = main",
         ],
     },
 )
