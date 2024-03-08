6b7901fba51aeba7befea914f7f94d4894bed4eb
Auric Vente <mprops@protonmail.com>
2024-03-07 21:11:17 -0600

    Mods

--- meltdown/timeutils.py
+++ meltdown/timeutils.py
@@ -10,9 +10,9 @@ def seconds_string(name: str, start: float, end: float) -> str:
 
 
 def check_time(name: str, last_time: float) -> Tuple[str, float]:
-    now = now()
-    seconds_str = seconds_string(name, now, last_time)
-    return seconds_str, now
+    time_now = now()
+    seconds_str = seconds_string(name, time_now, last_time)
+    return seconds_str, time_now
 
 
 def now() -> float:
@@ -24,8 +24,8 @@ def now_int() -> int:
 
 
 def date() -> str:
-    now = datetime.now()
-    return now.strftime("%Y-%m-%d %H:%M:%S")
+    time_now = datetime.now()
+    return time_now.strftime("%Y-%m-%d %H:%M:%S")
 
 
 def sleep(seconds: float) -> None:
