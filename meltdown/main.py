50dc4f3ce062f3c70b096feeea3d8be0bd39a8f1
Auric Vente <mprops@protonmail.com>
2024-03-11 23:20:43 -0600

    Mods

--- meltdown/main.py
+++ meltdown/main.py
@@ -3,7 +3,6 @@ from .app import app
 from .widgets import widgets
 from .model import model
 from .session import session
-from .menus import Menu
 from . import state
 from . import system
 
@@ -14,7 +13,6 @@ def main() -> None:
     widgets.setup()
     system.start()
     app.setup()
-    Menu.setup()
 
     try:
         app.run()
--- meltdown/menus.py
+++ meltdown/menus.py
@@ -24,16 +24,12 @@ class Menu:
     current_command: Optional[Callable[..., Any]] = None
 
     @staticmethod
-    def setup() -> None:
-        app.root.bind("<Button-1>", lambda e: Menu.hide_all())
-
-    @staticmethod
     def hide_all() -> None:
         if Menu.current_menu:
             Menu.current_menu.hide()
 
     def __init__(self) -> None:
-        self.menu: Optional[tk.Toplevel] = None
+        self.menu: Optional[tk.Frame] = None
         self.items: List[MenuItem] = []
 
     def add(self, text: str, command: Optional[Callable[..., Any]] = None, disabled: bool = False) -> None:
@@ -46,9 +42,7 @@ class Menu:
         self.items = []
 
     def make(self, event: Any) -> None:
-        self.menu = tk.Toplevel(app.root, bg="white")
-        self.menu.wm_overrideredirect(True)
-        self.menu.withdraw()
+        self.menu = tk.Frame(bg="white")
         self.menu.lift()
 
         Menu.current_widget = None
@@ -120,34 +114,25 @@ class Menu:
 
         self.menu.bind("<FocusOut>", lambda e: self.hide())
 
-        x = event.x_root
-        y = event.y_root
-
         self.menu.update_idletasks()
         window_width = app.root.winfo_width()
         window_height = app.root.winfo_height()
         menu_width = self.menu.winfo_reqwidth()
         menu_height = self.menu.winfo_reqheight()
-        window_x = app.root.winfo_x()
-        window_y = app.root.winfo_y()
-        left_edge = window_x
-        right_edge = window_x + window_width
-        top_edge = window_y
-        bottom_edge = window_y + window_height
-        x = event.x_root - (menu_width // 2)
-        y = event.y_root + 20
+        x = event.x_root - app.root.winfo_rootx()
+        y = event.y_root - app.root.winfo_rooty()
 
-        if x < left_edge:
-            x = left_edge
-        elif x + menu_width > right_edge:
-            x = right_edge - menu_width
+        if x < 0:
+            x = 0
+        elif x + menu_width > window_width:
+            x = window_width - menu_width
 
-        if y < top_edge:
-            y = top_edge
-        elif y + menu_height > bottom_edge:
-            y = event.y_root - menu_height
+        if y < 0:
+            y = 0
+        elif y + menu_height > window_height:
+            y = window_height - menu_height
 
-        self.menu.geometry("+%d+%d" % (x, y))
+        self.menu.place(x=x, y=y)
 
     def show(self, event: Any) -> None:
         Menu.hide_all()
@@ -155,10 +140,7 @@ class Menu:
         def do_show() -> None:
             if self.menu:
                 self.menu.update_idletasks()
-                self.menu.deiconify()
-                self.menu.transient(app.root)
-                self.menu.update()
-                self.menu.wait_window()
+                self.menu.focus_set()
 
         if not self.items:
             return
@@ -169,5 +151,6 @@ class Menu:
 
     def hide(self) -> None:
         if self.menu:
+            self.menu.place_forget()
             self.menu.destroy()
             Menu.current_menu = None
