83ae73aae08a6ebe1c7e317d3811c55d6570d717
Auric Vente <mprops@protonmail.com>
2024-03-17 21:57:50 -0600

    Mods

--- run.sh
+++ run.sh
@@ -1,4 +1,32 @@
 #!/usr/bin/env bash
 root="$(dirname "$(readlink -f "$0")")"
 cd "$root"
-venv/bin/python -m meltdown.main "$@"
+
+# Keep this running, to restart the program just press Up arrow key
+
+launch_program() {
+    venv/bin/python -m meltdown.main "$@" &
+    program_pid=$!
+}
+
+restart_program() {
+    kill -9 "$program_pid" >/dev/null 2>&1
+    launch_program
+}
+
+main() {
+    launch_program
+
+    while true; do
+        read -rsn1 key
+        if [[ "$key" == $'\x1b' ]]; then
+            read -rsn2 key
+            if [[ "$key" == '[A' ]]; then
+                echo "Restarting program..."
+                restart_program
+            fi
+        fi
+    done
+}
+
+main
