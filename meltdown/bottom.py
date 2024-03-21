7e01c8dc33f4e15b315575462cad6fab1c8d6545
Auric Vente <mprops@protonmail.com>
2024-03-21 08:37:05 -0600

    Mods

--- meltdown/bottom.py
+++ meltdown/bottom.py
@@ -16,20 +16,36 @@ class Bottom(tk.Frame):
         self.parent = parent
         self.tab_id = tab_id
         self.visible = True
+        self.delay = 500
+        self.show_debouncer = ""
         self.grid(row=1, column=0, sticky="nsew")
         self.grid_remove()
+
+    def do_show(self) -> None:
+        if not self.visible:
+            return
+
+        self.grid()
+
+    def cancel_show(self) -> None:
+        if not self.show_debouncer:
+            return
+
+        app.root.after_cancel(self.show_debouncer)
+        self.show_debouncer = ""
 
     def show(self) -> None:
         if self.visible or (not app.exists()):
             return
 
         self.visible = True
-        self.grid()
+        self.show_debouncer = app.root.after(self.delay, self.do_show)
 
     def hide(self) -> None:
         if (not self.visible) or (not app.exists()):
             return
 
+        self.cancel_show()
         self.visible = False
         self.grid_remove()
 
