abcb0016508c031bfa2036bf8f2a6a9ae48efaaa
Auric Vente <mprops@protonmail.com>
2024-04-19 11:43:54 -0600

    Mods

--- meltdown/args.py
+++ meltdown/args.py
@@ -124,7 +124,7 @@ class Args:
         self.progjson = ""
         self.program = ""
         self.gestures = True
-        self.gesture_threshold = 50
+        self.gesture_threshold = 33
 
     class Internal:
         title = app.manifest["title"]
