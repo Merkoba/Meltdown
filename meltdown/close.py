b670e590836a408cfac22daed59cfa7b604a3c4a
Auric Vente <mprops@protonmail.com>
2024-05-13 06:07:33 -0600

    Mods

--- meltdown/close.py
+++ meltdown/close.py
@@ -12,6 +12,61 @@ if TYPE_CHECKING:
     from .display import Tab
 
 
+def get_old_tabs() -> List["Tab"]:
+    ids = display.tab_ids()
+    old_tabs = []
+
+    max_minutes = args.old_tabs_minutes
+    max_date = utils.now() - (60 * max_minutes)
+
+    for tab_id in ids:
+        tabconvo = display.get_tab_convo(tab_id)
+
+        if not tabconvo:
+            continue
+
+        if tabconvo.convo.last_modified < max_date:
+            old_tabs.append(tabconvo.tab)
+
+    return old_tabs
+
+
+def get_empty_tabs() -> List["Tab"]:
+    ids = display.tab_ids()
+    empty_tabs = []
+
+    for tab_id in ids:
+        tabconvo = display.get_tab_convo(tab_id)
+
+        if not tabconvo:
+            continue
+
+        if not tabconvo.convo.items:
+            empty_tabs.append(tabconvo.tab)
+
+    return empty_tabs
+
+
+def get_left_tabs(tab_id: str) -> List[str]:
+    tab_ids = display.tab_ids()
+    index = display.index(tab_id)
+    return tab_ids[:index]
+
+
+def get_right_tabs(tab_id: str) -> List[str]:
+    tab_ids = display.tab_ids()
+    index = display.index(tab_id)
+    return tab_ids[index + 1 :]
+
+
+def get_other_tabs(tab_id: str) -> List[str]:
+    ids = display.tab_ids()
+    return [tid for tid in ids if tid != tab_id]
+
+
+# -----------------
+
+
 def close_tab(
     tab_id: Optional[str] = None,
     force: bool = False,
@@ -58,9 +113,9 @@ def close_tab(
     if full and get_old_tabs():
         cmds.append(("Old", lambda: close_old_tabs()))
 
-    cmds.append(("Others", lambda: close_other_tabs()))
-    cmds.append(("Left", lambda: close_tabs_left()))
-    cmds.append(("Right", lambda: close_tabs_right()))
+    cmds.append(("Others", lambda: close_other_tabs(tab_id=tab_id)))
+    cmds.append(("Left", lambda: close_tabs_left(tab_id=tab_id)))
+    cmds.append(("Right", lambda: close_tabs_right(tab_id=tab_id)))
 
     if full:
         cmds.append(("All", lambda: close_all_tabs()))
@@ -80,25 +135,6 @@ def close_all_tabs(force: bool = False, make_empty: bool = True) -> None:
 
     n = display.num_tabs()
     Dialog.show_confirm(f"Close all tabs ({n}) ?", lambda: action())
-
-
-def get_old_tabs() -> List["Tab"]:
-    ids = display.tab_ids()
-    old_tabs = []
-
-    max_minutes = args.old_tabs_minutes
-    max_date = utils.now() - (60 * max_minutes)
-
-    for tab_id in ids:
-        tabconvo = display.get_tab_convo(tab_id)
-
-        if not tabconvo:
-            continue
-
-        if tabconvo.convo.last_modified < max_date:
-            old_tabs.append(tabconvo.tab)
-
-    return old_tabs
 
 
 def close_old_tabs(force: bool = False) -> None:
@@ -124,14 +160,11 @@ def close_old_tabs(force: bool = False) -> None:
     Dialog.show_confirm(f"Close old tabs ({n}) ?", lambda: action())
 
 
-def close_other_tabs(force: bool = False) -> None:
-    ids = display.tab_ids()
+def close_other_tabs(force: bool = False, tab_id: Optional[str] = None) -> None:
+    if not tab_id:
+        tab_id = display.current_tab
 
-    if len(ids) <= 1:
-        return
-
-    current = display.current_tab
-    tab_ids = [tab_id for tab_id in ids if tab_id != current]
+    tab_ids = get_other_tabs(tab_id)
 
     def action() -> None:
         for tab_id in tab_ids:
@@ -145,62 +178,46 @@ def close_other_tabs(force: bool = False) -> None:
     Dialog.show_confirm(f"Close other tabs ({n}) ?", lambda: action())
 
 
-def close_tabs_left(force: bool = False) -> None:
-    tab_ids = display.tab_ids()
+def close_tabs_left(force: bool = False, tab_id: Optional[str] = None) -> None:
+    if not tab_id:
+        tab_id = display.current_tab
 
-    if len(tab_ids) <= 1:
+    tab_ids = get_left_tabs(tab_id)
+
+    if not tab_ids:
         return
 
-    index = display.index(display.current_tab)
-    tabs = tab_ids[:index]
-
     def action() -> None:
-        for tab_id in tabs:
+        for tab_id in tab_ids:
             close_tab(tab_id=tab_id, force=True)
 
     if force or (not args.confirm_close):
         action()
         return
 
-    n = len(tabs)
+    n = len(tab_ids)
     Dialog.show_confirm(f"Close tabs to the left ({n}) ?", lambda: action())
 
 
-def close_tabs_right(force: bool = False) -> None:
-    tab_ids = display.tab_ids()
+def close_tabs_right(force: bool = False, tab_id: Optional[str] = None) -> None:
+    if not tab_id:
+        tab_id = display.current_tab
 
-    if len(tab_ids) <= 1:
+    tab_ids = get_right_tabs(tab_id)
+
+    if not tab_ids:
         return
 
-    index = display.index(display.current_tab)
-    tabs = tab_ids[index + 1 :]
-
     def action() -> None:
-        for tab_id in tabs:
+        for tab_id in tab_ids:
             close_tab(tab_id=tab_id, force=True)
 
     if force or (not args.confirm_close):
         action()
         return
 
-    n = len(tabs)
+    n = len(tab_ids)
     Dialog.show_confirm(f"Close tabs to the right ({n}) ?", lambda: action())
-
-
-def get_empty_tabs() -> List["Tab"]:
-    ids = display.tab_ids()
-    empty_tabs = []
-
-    for tab_id in ids:
-        tabconvo = display.get_tab_convo(tab_id)
-
-        if not tabconvo:
-            continue
-
-        if not tabconvo.convo.items:
-            empty_tabs.append(tabconvo.tab)
-
-    return empty_tabs
 
 
 def close_empty_tabs(force: bool = False) -> None:
