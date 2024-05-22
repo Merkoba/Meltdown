cceb3df33dfaa8ddd503a55e4d363720b353d93c
Auric Vente <mprops@protonmail.com>
2024-05-22 07:36:31 -0600

    Mods

--- meltdown/args.py
+++ meltdown/args.py
@@ -203,7 +203,7 @@ class Args:
         self.border_size = 5
         self.title = ""
         self.icon = ""
-        self.auto_scroll_delay = 500
+        self.auto_scroll_delay = 720
         self.show_auto_scroll = True
         self.scroller_buttons = True
         self.portrait = ""
