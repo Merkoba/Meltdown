b16d8e62173c0d7899ff54cdc0879f90d5ea4d85
Auric Vente <mprops@protonmail.com>
2024-05-22 00:47:05 -0600

    Mods

--- scripts/tag.py
+++ scripts/tag.py
@@ -7,14 +7,18 @@
 import os
 import git
 import time
+import json
 from pathlib import Path
 
 here = Path(__file__).resolve()
 parent = here.parent.parent
 os.chdir(parent)
 
-name = int(time.time())
+with open("meltdown/manifest.json") as f:
+    manifest = json.loads(f.read())
+
+version = manifest["version"]
 repo = git.Repo(".")
-repo.create_tag(name)
-repo.remotes.origin.push(name)
-print(f"Created tag: {name}")
+repo.create_tag(version)
+repo.remotes.origin.push(version)
+print(f"Created tag: {version}")
