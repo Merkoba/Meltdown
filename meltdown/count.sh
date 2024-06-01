1289afcdde57f99d9c22c981bf86655ee4187309
Auric Vente <mprops@protonmail.com>
2024-05-31 18:52:12 -0600

    Mods

--- meltdown/count.sh
+++ meltdown/count.sh
@@ -1,2 +1,2 @@
 # Count the total number of lines
-find . -type f -name "*.py" ! -name "pyperclip.py" -exec wc -l {} \; | awk '{t+=$1} END {print t}'
+find . -type f -name "*.py" ! -name "pyperclip.py" ! -name "tests.py" -exec wc -l {} \; | awk '{t+=$1} END {print t}'
