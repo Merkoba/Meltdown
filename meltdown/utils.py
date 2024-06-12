ee378ed6305d2ea28fb64ddf0b8896dd6d616e00
Auric Vente <mprops@protonmail.com>
2024-06-12 00:07:04 -0600

    Mods

--- meltdown/utils.py
+++ meltdown/utils.py
@@ -107,8 +107,9 @@ class Utils:
         error_handler.setFormatter(formatter)
 
     def seconds_string(self, name: str, start: float, end: float) -> str:
-        num = round(start - end, 2)
-        return f"{name} in {num} seconds"
+        num = round(start - end, 1)
+        m = self.singular_or_plural(num, "sec", "secs")
+        return f"{name} ({num} {m})"
 
     def check_time(self, name: str, last_time: float) -> tuple[str, float]:
         time_now = self.now()
