bd85f6200a359797dfb04c98179ec0d90b5c5ce2
Auric Vente <mprops@protonmail.com>
2025-01-12 04:56:24 -0600

    Mods

--- meltdown/paths.py
+++ meltdown/paths.py
@@ -77,6 +77,9 @@ class Paths:
         self.openai_key = Path(self.data_dir, "openai_key.txt")
         self.google_key = Path(self.data_dir, "google_key.txt")
         self.errors = Path(self.data_dir, "errors")
+
+        # Assets
+
         self.nouns = Path(app.here, "nouns.txt")
         return True
 
